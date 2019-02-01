b6901549c27afc0abc2a6171b988e52cf3d341ce
s1mple <798342418@qq.com>
2019-02-01 16:28:30 +0800

    assert Point methods

--- src/test/java/com/caacetc/hackathon/routeplan/PointTest.java
+++ src/test/java/com/caacetc/hackathon/routeplan/PointTest.java
@@ -7,10 +7,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PointTest {
     @Test
-    void should_point_methods_correct() {
+    void should_point_methods_run_correctly() {
         assertThat(A.isS() || A.isV()).isFalse();
         assertThat(S1.isS()).isTrue();
         assertThat(S2.isV()).isFalse();
         assertThat(V3.isV()).isTrue();
+        assertThat(S1.matchedV()).isEqualTo(V1);
+        assertThat(S3.matchedV()).isEqualTo(V3);
     }
 }
---
+++ src/test/java/com/caacetc/hackathon/routeplan/RouteTest.java
@@ -0,0 +1,16 @@
+package com.caacetc.hackathon.routeplan;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RouteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void should_() {
+
+    }
+}
