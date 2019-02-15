26ae7b8929a096372072da125bc2bb7d994c6e71
s1mple <798342418@qq.com>
2019-02-15 15:05:48 +0800

    add should_compute_two_points_shortest_distance test

--- src/test/java/com/caacetc/hackathon/routeplan/DijkstraTest.java
+++ src/test/java/com/caacetc/hackathon/routeplan/DijkstraTest.java
@@ -2,9 +2,20 @@ package com.caacetc.hackathon.routeplan;
 
 import org.junit.jupiter.api.Test;
 
-class DijkstraTest {
-    @Test
-    public void should_() {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class DijkstraTest {
+    private Dijkstra dijkstra = new Dijkstra(new Graph());
+
+    @Test
+    void should_compute_two_points_shortest_distance() {
+        assertThat(dijkstra.min(1, 3)).isEqualTo(3);
+        assertThat(dijkstra.min(1, 8)).isEqualTo(4);
+        assertThat(dijkstra.min(1, 9)).isEqualTo(2);
+        assertThat(dijkstra.min(1, 10)).isEqualTo(4);
+        assertThat(dijkstra.min(1, 15)).isEqualTo(7);
+        assertThat(dijkstra.min(3, 14)).isEqualTo(4);
+        assertThat(dijkstra.min(3, 15)).isEqualTo(4);
+        assertThat(dijkstra.min(4, 11)).isEqualTo(4);
     }
 }
